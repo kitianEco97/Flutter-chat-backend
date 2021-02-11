9fa122ded58f437b5e205ce676c64691a06c6d5a
Kitian Morales <christianmosa123321@gmail.com>
2021-02-11 20:44:30 -0300

    fin seccion 8

---
+++ controllers/auth.js
@@ -0,0 +1,114 @@
+/*
+    path: api/login
+*/ 
+const { response } = require('express');
+const bcrypt = require('bcryptjs');
+
+const Usuario = require('../models/usuario');
+const { generarJWT } = require('../helpers/jwt');
+
+const crearUsuario = async (req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        const existeEmail = await Usuario.findOne({ email })
+        if (existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'el correo ya esta registrado'
+            });
+        }
+
+        const usuario =  new Usuario( req.body );
+
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync( password, salt );
+
+        await usuario.save();
+
+        // Generar JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            ok: true,
+            usuario,
+            token
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'hable con el admin'
+        });
+    }
+
+}
+
+const login = async (req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        const usuarioDB = await Usuario.findOne({email});
+        if ( !usuarioDB ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Email no encontrado'
+            });
+        }
+
+        // Validar password
+        const validPassword = bcrypt.compareSync( password, usuarioDB.password );
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'La contraseña no es valida'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuarioDB.id );
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token
+        });
+        
+    } catch (error) {        
+        console.log(error);
+        return res.json({
+            ok: false,
+            msg: 'Halbe con el admin'
+        });
+    }
+
+}
+
+const renewToken = async ( req, res = response ) => {
+
+    const uid = req.uid;
+
+    const token = await generarJWT(uid);
+
+    // obtener el usuario por el uid, usuario.findbyid;
+    const usuario = await Usuario.findById(uid);
+
+    res.json({
+        ok: true,
+        usuario,
+        token
+    })
+
+}
+
+module.exports = {
+    crearUsuario,
+    login,
+    renewToken
+}
